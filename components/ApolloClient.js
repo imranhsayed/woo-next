9c700cd25b5e439de529aa625b8f8167defd86aa
Imran Sayed <imranhsayed@gmail.com>
2020-03-31 13:42:00 +0530

    Add the middleware and afterware

--- components/ApolloClient.js
+++ components/ApolloClient.js
@@ -2,6 +2,7 @@ import fetch from 'node-fetch';
 import { ApolloClient } from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { createHttpLink } from 'apollo-link-http';
+import { ApolloLink } from "apollo-link";
 
 import { IntrospectionFragmentMatcher } from 'apollo-cache-inmemory';
 import introspectionQueryResultData from '../fragmentTypes';
@@ -13,12 +14,69 @@ const fragmentMatcher = new IntrospectionFragmentMatcher({
 	introspectionQueryResultData
 });
 
+/**
+ * Middleware operation
+ * If we have a session token in localStorage, add it to the GraphQL request as a Session header.
+ */
+export const middleware = new ApolloLink( ( operation, forward ) => {
+	/**
+	 * If session data exist in local storage, set value as session header.
+	 */
+	const session = ( process.browser ) ?  localStorage.getItem( "woo-session" ) : null;
+
+	if ( session ) {
+		operation.setContext( ( { headers = {} } ) => ( {
+			headers: {
+				"woocommerce-session": `Session ${ session }`
+			}
+		} ) );
+	}
+
+	return forward( operation );
+
+} );
+
+/**
+ * Afterware operation.
+ *
+ * This catches the incoming session token and stores it in localStorage, for future GraphQL requests.
+ */
+export const afterware = new ApolloLink( ( operation, forward ) => {
+
+	return forward( operation ).map( response => {
+		/**
+		 * Check for session header and update session in local storage accordingly.
+		 */
+		const context = operation.getContext();
+		const { response: { headers } }  = context;
+		const session = headers.get( "woocommerce-session" );
+
+		if ( session ) {
+
+			// Remove session data if session destroyed.
+			if ( "false" === session ) {
+
+				localStorage.removeItem( "woo-session" );
+
+				// Update session new data if changed.
+			} else if ( localStorage.getItem( "woo-session" ) !== session ) {
+
+				localStorage.setItem( "woo-session", headers.get( "woocommerce-session" ) );
+
+			}
+		}
+
+		return response;
+
+	} );
+} );
+
 // Apollo GraphQL client.
 const client = new ApolloClient({
-	link: createHttpLink({
+	link: middleware.concat( afterware.concat( createHttpLink({
 		uri: clientConfig.graphqlUrl,
 		fetch: fetch
-	}),
+	}) ) ),
 	cache: new InMemoryCache( { fragmentMatcher } ),
 });
 
