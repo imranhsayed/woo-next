a455002035a434a119dbdf4f138212cfcb8c3fd7
Imran <imranhsayed@gmail.com>
2020-11-29 09:32:35 +0530

    Update links

--- components/Nav.js
+++ components/Nav.js
@@ -27,12 +27,16 @@ const Nav = () => {
 
 				<div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
 					<div className="text-sm font-medium uppercase lg:flex-grow">
-					<a href="/categories" className="block mt-4 lg:inline-block lg:mt-0 text-black hover:text-black mr-10">
-						Men
-					</a>
-					<a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-black hover:text-black mr-10">
-						Women
-					</a>
+					<Link href="/categories">
+						<a className="block mt-4 lg:inline-block lg:mt-0 text-black hover:text-black mr-10">
+							Categories
+						</a>
+					</Link>
+					<Link href="/cart">
+						<a className="block mt-4 lg:inline-block lg:mt-0 text-black hover:text-black mr-10">
+							Cart
+						</a>
+					</Link>
 					<a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-black hover:text-black mr-10">
 						Kids
 					</a>
