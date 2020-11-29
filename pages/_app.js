05612e0af70b5ccc7d59d5ba980db93d274e768c
Imran <imranhsayed@gmail.com>
2020-11-29 09:25:43 +0530

    Add nprogress

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,14 @@
 import "../styles/Style.scss";
 import "../styles/main.scss";
 
+import Router from 'next/router';
+import NProgress from 'nprogress';
+
+NProgress.configure({ showSpinner: false });
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
