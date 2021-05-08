f24ed0ddee32a70bee06c01233652d0a870aa3cb
Imran <imranhsayed@gmail.com>
2021-05-08 12:39:25 +0530

    Update styles

--- src/components/checkout/Address.js
+++ src/components/checkout/Address.js
@@ -16,90 +16,91 @@ const Address = ({ input, countries, states, handleOnChange } ) => {
 			<div className="">
 				<div className="">
 					<div className="form-group mb-3">
-						<label className="text-xs" htmlFor="first-name">
+						<label className="leading-7 text-sm text-gray-600" htmlFor="first-name">
 							First Name
 							<abbr className="required" title="required">*</abbr>
 						</label>
-						<input onChange={ handleOnChange } value={ input.firstName } type="text" name="firstName" className="form-control woo-next-checkout-input p-1 w-full border-solid border border-gray-500 rounded" id="first-name"/>
+						<input onChange={ handleOnChange } value={ input.firstName } type="text" name="firstName" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-400 focus:border-indigo-500 focus:bg-transparent focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" id="first-name"/>
 						<Error errors={ input.errors } fieldName={ 'firstName' }/>
 					</div>
 				</div>
 				<div className="">
 					<div className="form-group mb-3">
-						<label className="text-xs" htmlFor="last-name">
+						<label className="leading-7 text-sm text-gray-600" htmlFor="last-name">
 							Last Name
 							<abbr className="required" title="required">*</abbr>
 						</label>
-						<input onChange={ handleOnChange } value={ input.lastName } type="text" name="lastName" className="form-control woo-next-checkout-input p-1 w-full border-solid border border-gray-500 rounded" id="last-name"/>
+						<input onChange={ handleOnChange } value={ input.lastName } type="text" name="lastName" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-400 focus:border-indigo-500 focus:bg-transparent focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" id="last-name"/>
 						<Error errors={ input.errors } fieldName={ 'lastName' }/>
 					</div>
 				</div>
 			</div>
 			{/* Company Name */}
 			<div className="form-group mb-3">
-				<label className="text-xs" htmlFor="first-name">Company Name</label>
-				<input onChange={ handleOnChange } value={ input.company } type="text" name="company" className="form-control woo-next-checkout-input p-1 w-full border-solid border border-gray-500 rounded" id="first-name"/>
+				<label className="leading-7 text-sm text-gray-600" htmlFor="first-name">Company Name</label>
+				<input onChange={ handleOnChange } value={ input.company } type="text" name="company" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-400 focus:border-indigo-500 focus:bg-transparent focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" id="first-name"/>
 				<Error errors={ input.errors } fieldName={ 'company' }/>
 			</div>
 			{/* Country */}
 			<CountrySelection input={input} handleOnChange={handleOnChange} countries={countries}/>
 			{/* Street Address */}
 			<div className="form-group mb-3">
-				<label className="text-xs" htmlFor="street-address">
+				<label className="leading-7 text-sm text-gray-600" htmlFor="street-address">
 					Street Address
 					<abbr className="required" title="required">*</abbr>
 				</label>
-				<input type="text" onChange={ handleOnChange } value={ input.address1 } name="address1" placeholder="House number and street name" className="form-control woo-next-checkout-input p-1 w-full border-solid border border-gray-500 rounded mb-3" id="street-address"/>
+				<input type="text" onChange={ handleOnChange } value={ input.address1 } name="address1" placeholder="House number and street name" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-400 focus:border-indigo-500 focus:bg-transparent focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" id="street-address"/>
 				<Error errors={ input.errors } fieldName={ 'address1' }/>
 				<br/>
-				<input type="text" onChange={ handleOnChange } value={ input.address2 } name="address2" placeholder="Apartment, suite, unit etc.(optional)" className="form-control woo-next-checkout-input p-1 w-full border-solid border border-gray-500 rounded" id="first-name"/>
+				<br/>
+				<input type="text" onChange={ handleOnChange } value={ input.address2 } name="address2" placeholder="Apartment, suite, unit etc.(optional)" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-400 focus:border-indigo-500 focus:bg-transparent focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" id="first-name"/>
 			</div>
 			{/* Town/City */}
 			<div className="form-group mb-3">
-				<label className="text-xs" htmlFor="city">
+				<label className="leading-7 text-sm text-gray-600" htmlFor="city">
 					Town/City
 					<abbr className="required" title="required">*</abbr>
 				</label>
-				<input onChange={ handleOnChange } value={ input.city } type="text" name="city" className="form-control woo-next-checkout-input p-1 w-full border-solid border border-gray-500 rounded" id="city"/>
+				<input onChange={ handleOnChange } value={ input.city } type="text" name="city" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-400 focus:border-indigo-500 focus:bg-transparent focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" id="city"/>
 				<Error errors={ input.errors } fieldName={ 'city' }/>
 			</div>
 			{/* State */}
 			<StateSelection input={input} handleOnChange={handleOnChange} states={statesData}/>
 			{/* Post Code */}
 			<div className="form-group mb-3">
-				<label className="text-xs" htmlFor="post-code">
+				<label className="leading-7 text-sm text-gray-600" htmlFor="post-code">
 					Postcode
 					<abbr className="required" title="required">*</abbr>
 				</label>
-				<input onChange={ handleOnChange } value={ input.postcode } type="text" name="postcode" className="form-control woo-next-checkout-input p-1 w-full border-solid border border-gray-500 rounded" id="post-code"/>
+				<input onChange={ handleOnChange } value={ input.postcode } type="text" name="postcode" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-400 focus:border-indigo-500 focus:bg-transparent focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" id="post-code"/>
 				<Error errors={ input.errors } fieldName={ 'postcode' }/>
 			</div>
 			{/*Phone & Email*/}
 			<div className="row">
 				<div className="col-lg-6 col-md-12 p-0 pr-2">
 					<div className="form-group mb-3">
-						<label className="text-xs" htmlFor="phone">
+						<label className="leading-7 text-sm text-gray-600" htmlFor="phone">
 							Phone
 							<abbr className="required" title="required">*</abbr>
 						</label>
-						<input onChange={ handleOnChange } value={ input.phone } type="text" name="phone" className="form-control woo-next-checkout-input p-1 w-full border-solid border border-gray-500 rounded" id="phone"/>
+						<input onChange={ handleOnChange } value={ input.phone } type="text" name="phone" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-400 focus:border-indigo-500 focus:bg-transparent focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" id="phone"/>
 						<Error errors={ input.errors } fieldName={ 'phone' }/>
 					</div>
 				</div>
 				<div className="col-lg-6 col-sm-12 p-0">
 					<div className="form-group mb-3">
-						<label className="text-xs" htmlFor="email">
+						<label className="leading-7 text-sm text-gray-600" htmlFor="email">
 							Email
 							<abbr className="required" title="required">*</abbr>
 						</label>
-						<input onChange={ handleOnChange } value={ input.email } type="email" name="email" className="form-control woo-next-checkout-input p-1 w-full border-solid border border-gray-500 rounded" id="email"/>
+						<input onChange={ handleOnChange } value={ input.email } type="email" name="email" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-400 focus:border-indigo-500 focus:bg-transparent focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" id="email"/>
 						<Error errors={ input.errors } fieldName={ 'email' }/>
 					</div>
 				</div>
 			</div>
 			{/*	@TODO Create an Account */}
 			{/*<div className="form-check">*/}
-			{/*	<label className="text-xs" className="form-check-label">*/}
+			{/*	<label className="leading-7 text-sm text-gray-600" className="form-check-label">*/}
 			{/*		<input onChange={ handleOnChange } className="form-check-input" name="createAccount" type="checkbox"/>*/}
 			{/*			Create an account?*/}
 			{/*	</label>*/}
@@ -107,7 +108,7 @@ const Address = ({ input, countries, states, handleOnChange } ) => {
 			{/*<h2 className="mt-4 mb-4">Additional Information</h2>*/}
 			{/* @TODO Order Notes */}
 			{/*<div className="form-group mb-3">*/}
-			{/*	<label className="text-xs" htmlFor="order-notes">Order Notes</label>*/}
+			{/*	<label className="leading-7 text-sm text-gray-600" htmlFor="order-notes">Order Notes</label>*/}
 			{/*	<textarea onChange={ handleOnChange } defaultValue={ input.orderNotes } name="orderNotes" className="form-control woo-next-checkout-textarea" id="order-notes" rows="4"/>*/}
 			{/*	<Error errors={ input.errors } fieldName={ 'orderNotes' }/>*/}
 			{/*</div>*/}
