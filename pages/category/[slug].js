3941f857a8c3a707e4fb24fc3e6b93ca9dc892cb
Imran <imranhsayed@gmail.com>
2021-05-08 11:26:42 +0530

    Update categories style

--- pages/category/[slug].js
+++ pages/category/[slug].js
@@ -21,7 +21,7 @@ export default function CategorySingle( props ) {
         <Layout>
             <div className="product-categories-container container mx-auto my-32 px-4 xl:px-0">
                 { categoryName ? <h3 className="text-2xl mb-5 uppercase">{ categoryName }</h3> : '' }
-                <div className="product-categories grid grid-cols-3 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-4">
+                <div className="product-categories grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-4">
                     { undefined !== products && products?.length ? (
                         products.map( product => <Product key={ product?.id } product={ product } /> )
                     ) : ''}
@@ -42,8 +42,8 @@ export async function getStaticProps(context) {
 
     return {
         props: {
-            categoryName: data?.productCategory?.name || '',
-            products: data?.productCategory?.products?.nodes || []
+            categoryName: data?.productCategory?.name ?? '',
+            products: data?.productCategory?.products?.nodes ?? []
         },
         revalidate: 1
     }
