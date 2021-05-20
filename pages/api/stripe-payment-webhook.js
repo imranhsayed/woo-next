6aca2ed6612f3ad0af6c4d795be5333b514940c5
Imran <imranhsayed@gmail.com>
2021-05-20 15:47:41 +0530

    Update stripe

--- pages/api/stripe-payment-webhook.js
+++ pages/api/stripe-payment-webhook.js
@@ -2,3 +2,25 @@
 /**
  * @TODO Build a webhook that updates the order status.
  */
+export default async function handler(req, res) {
+    const event = req.body;
+    // Handle the event
+    switch (event.type) {
+        case 'payment_intent.succeeded':
+            const paymentIntent = event.data.object;
+            console.log(`PaymentIntent for ${paymentIntent.amount} was successful!`);
+            // Then define and call a method to handle the successful payment intent.
+            // handlePaymentIntentSucceeded(paymentIntent);
+            break;
+        case 'payment_method.attached':
+            const paymentMethod = event.data.object;
+            // Then define and call a method to handle the successful attachment of a PaymentMethod.
+            // handlePaymentMethodAttached(paymentMethod);
+            break;
+        default:
+            // Unexpected event type
+            console.log(`Unhandled event type ${event.type}.`);
+    }
+    // Return a 200 response to acknowledge receipt of the event
+    res.status(200).json({hello: 'hey'});
+}
